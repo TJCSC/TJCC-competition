efc129b26d03308ee43c689b759f77baf9484ab8
Arlan Jaska <akjaska@gmail.com>
2012-03-23 15:01:39 -0400

    Creates ./files/ directory if it does not exist

--- server.py
+++ server.py
@@ -3,9 +3,14 @@
 from CGIHTTPServer import CGIHTTPRequestHandler as RH
 from BaseHTTPServer import HTTPServer as HS
 import cgitb; cgitb.enable()    #enables error reporting
+import os
 
 class Handler(RH):
     cgi_directories = ["/cgi"]
+
+if not os.path.exists("./files"):
+    print "Creating ./files directory"
+    os.makedirs("./files")
 
 port = 8080
 
