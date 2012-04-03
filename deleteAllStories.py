3149e2aa6bdd0574a19b542c3f28a6458321decc
Joe Gray <akjaska@gmail.com>
2012-04-03 16:42:53 -0400

    Updated deleteAllStories.py to work on Windows

--- deleteAllStories.py
+++ deleteAllStories.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 
 import sqlite3 as sql
-from subprocess import call
+#from subprocess import call
+from shutil import rmtree
 
 def main():
 
     if raw_input("Are you sure you want to do this? (y/n) ") != "y":
         exit()
 
-    call("rm files/*", shell=True)
-    print 'Deleted pictures'
+#    call("rm files/*", shell=True)
+    try:
+        rmtree("./files/")
+    except OSError:
+        print 'Error: files/ did not exist'
+    else:
+        print 'Deleted pictures'
 
     with sql.connect('./database') as connection:
         d = connection.cursor()
