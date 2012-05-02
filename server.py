60ff7cab27a2b867abcd4bb6c3992eb779132bfe
Arlan Jaska <akjaska@gmail.com>
2012-05-02 15:37:25 -0400

    Removed select from server.py

--- server.py
+++ server.py
@@ -4,9 +4,6 @@ from CGIHTTPServer import CGIHTTPRequestHandler as RH
 from BaseHTTPServer import HTTPServer as HS
 import cgitb; cgitb.enable()    #enables error reporting
 import os
-import select
-import sys
-
 
 class Handler(RH):
     cgi_directories = ["/cgi"]
@@ -24,22 +21,9 @@ init()
 port = 8080
 httpd = HS(('',port), Handler)
 
-running = True
-while running:
+while True:
     try:
-        inready,outready,eready = select.select([httpd,sys.stdin],[],[])   # check for input from multiple sources
-        for s in inready:
-            if s == httpd: # input from HTTPServer
-                httpd.handle_request()
-            elif s == sys.stdin: # input from console
-                line =  sys.stdin.readline().split()
-                if line[0] in ["quit","exit","close","stop","halt"]:
-                    running = False
-                elif line[0] in ["delete","remove"]:
-                    import deleteAllStories
-                    if not deleteAllStories.main(): # re-create files/ to prevent errors
-                        init()
-                    
+        httpd.handle_request()
     except KeyboardInterrupt: # handle Ctrl-C properly
         break
 
